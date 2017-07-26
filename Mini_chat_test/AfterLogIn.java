7eaeb06690590727103c33b3fe0d659591163ea5
HelAsia <asiaheluszka1993@gmail.com>
2017-07-26 18:37:39 +0200

    Change code

--- Mini_chat_test/AfterLogIn.java
+++ Mini_chat_test/AfterLogIn.java
@@ -2,24 +2,20 @@ package Mini_chat_test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class AfterLogIn{
 	
-	public void test1 (String databaseDriver, String databaseUrl, String username, String password){
-		
-	try
-	   {
-		databaseDriver = "com.mysql.jdbc.Driver";
-		databaseUrl = "jdbc:mysql://localhost:3306/mini_chat";
+	public void test1 (String url, String username, String password){
+		url = "jdbc:mysql://localhost:3306/mini_chat";
 		username = "root";
 		password= "";
 		
-	    Class.forName(databaseDriver);
-	    Connection connection = DriverManager.getConnection(databaseUrl);
-	    String query = "Select * from TEST";
-	    Object resultSet = statement.executeQuery(query); 
-	    }catch(Exception ex){
-	   System.out.println("Connection Error");
-	  }
+		try (Connection connection = DriverManager.getConnection(url, username, password)){
+			 System.out.println("Database connected!");
+		} catch (SQLException e) {
+		    throw new IllegalStateException("Cannot connect the database!", e);
+	
+		}
 	}
 }
