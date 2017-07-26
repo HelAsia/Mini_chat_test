d8a670543d6774a5d7cb9cdb0f40cde264868c3f
HelAsia <asiaheluszka1993@gmail.com>
2017-07-26 21:31:11 +0200

    Add method connection in class MainScreen.java

--- Mini_chat_test/Main.java
+++ Mini_chat_test/Main.java
@@ -2,7 +2,7 @@ package Mini_chat_test;
 
 public class Main {
 	public static void main(String args[]){
-		AfterLogIn test = new AfterLogIn();
+		MainScreen test = new MainScreen();
 		test.connection("jdbc:mysql://localhost:3306/mini_chat","root","");
 	}
 }
--- Mini_chat_test/MainScreen.java
+++ Mini_chat_test/MainScreen.java
@@ -1,5 +1,20 @@
 package Mini_chat_test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Statement;
+
 public class MainScreen{
 	
+	public void connection (String url, String username, String password){
+
+		try (Connection connection = DriverManager.getConnection(url, username, password)){
+			 System.out.println("Database connected!");
+		} catch (SQLException e) {
+			throw new IllegalStateException("Cannot connect the database!", e);
+		}
+	}
+
 }
