0b4291bc4946a2b56aa7135629d16eddf6d5a91b
HelAsia <asiaheluszka1993@gmail.com>
2017-07-26 19:40:30 +0200

    Add class connection

--- Mini_chat_test/AfterLogIn.java
+++ Mini_chat_test/AfterLogIn.java
@@ -6,12 +6,13 @@ import java.sql.SQLException;
 
 public class AfterLogIn{
 	
-	public void test1 (String url, String username, String password){
-		url = "jdbc:mysql://localhost:3306/mini_chat";
-		username = "root";
-		password= "";
+	public void connection (String url, String username, String password){
+	//	url = "jdbc:mysql://localhost:3306/mini_chat";
+	//	username = "root";
+	//	password= "";
 		
 		try (Connection connection = DriverManager.getConnection(url, username, password)){
+			
 			 System.out.println("Database connected!");
 		} catch (SQLException e) {
 		    throw new IllegalStateException("Cannot connect the database!", e);
--- Mini_chat_test/Main.java
+++ Mini_chat_test/Main.java
@@ -3,6 +3,6 @@ package Mini_chat_test;
 public class Main {
 	public static void main(String args[]){
 		AfterLogIn test = new AfterLogIn();
-		test.test1();
+		test.connection("jdbc:mysql://localhost:3306/mini_chat","root","");
 	}
 }
