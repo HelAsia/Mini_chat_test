91e465b76eed8164a393cd5880c448ac6d50a410
HelAsia <asiaheluszka1993@gmail.com>
2017-08-16 20:36:47 +0200

    Add more option in if..else and add try...catch

--- Mini_chat_test/ChoiceWindow.java
+++ Mini_chat_test/ChoiceWindow.java
@@ -13,28 +13,43 @@ public class ChoiceWindow {
 	private MainScreen ms = new MainScreen();	
 	
 	public void window (){
+
+		
+		boolean trueOrFalse = true;
+		
 		System.out.println("What would you like to do?\n"
-				+ "If you would like to REGISTER you have to entered: 1 \n"
-				+ "If you would like to LOG IN you have to entered: 2 \n"
-				+ "If you would like to EXIT you have to entered: 3");
-		
-		Scanner number = new Scanner(System.in);
-		int userChoice = number.nextInt();
-		int ONE = 1;
-		int TWO = 2;
-		int THREE = 3;
-		
-		if (userChoice == ONE){
-			ms.register();
+				+ "[1] If you would like to REGISTER. \n"
+				+ "[2] If you would like to LOG IN. \n"
+				+ "[3] If you would like to EXIT. \n");
+		try{
+			do{
+				Scanner number = new Scanner(System.in);
+				int userChoice = number.nextInt();
+				int ONE = 1;
+				int TWO = 2;
+				int THREE = 3;
+				
+				if (userChoice == ONE){
+					ms.register();
+					trueOrFalse = false;
+				}
+				else if (userChoice == TWO){
+					ms.logIn();
+					trueOrFalse = false;
+				}
+				else if (userChoice ==THREE){
+					System.out.println("Your window will be close");
+					trueOrFalse = false;
+				}
+				else{
+					System.out.println("Your choice is wrong. Try again");
+					trueOrFalse = true;
+				}
+			}
+			while (trueOrFalse = true);
 		}
-		else if (userChoice == TWO){
-			ms.logIn();
-		}
-		else if (userChoice ==THREE){
-			number.close();
-		}
-		else{
-			System.out.println("Your choice is wrong. Try again");
+		catch (java.util.InputMismatchException e){
+			System.out.println("Your value is wrong format. Try again");
 		}
 	}
 }
